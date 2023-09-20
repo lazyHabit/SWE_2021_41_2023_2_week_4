4178004a8fb9551f1c7792d4ebe906625cb12c63
DoNotDelay <soleid1498@naver.com>
2023-09-20 11:26:16 +0000

    week4_assignment

---
+++ arrange.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+files_dir="/home/workspace/SWE_2021_41_2023_2_week_4-main/files"
+
+a_dir="/home/workspace/SWE_2021_41_2023_2_week_4-main/a"
+b_dir="/home/workspace/SWE_2021_41_2023_2_week_4-main/b"
+c_dir="/home/workspace/SWE_2021_41_2023_2_week_4-main/c"
+d_dir="/home/workspace/SWE_2021_41_2023_2_week_4-main/d"
+e_dir="/home/workspace/SWE_2021_41_2023_2_week_4-main/e"
+f_dir="/home/workspace/SWE_2021_41_2023_2_week_4-main/f"
+g_dir="/home/workspace/SWE_2021_41_2023_2_week_4-main/g"
+h_dir="/home/workspace/SWE_2021_41_2023_2_week_4-main/h"
+i_dir="/home/workspace/SWE_2021_41_2023_2_week_4-main/i"
+j_dir="/home/workspace/SWE_2021_41_2023_2_week_4-main/j"
+k_dir="/home/workspace/SWE_2021_41_2023_2_week_4-main/k"
+l_dir="/home/workspace/SWE_2021_41_2023_2_week_4-main/l"
+m_dir="/home/workspace/SWE_2021_41_2023_2_week_4-main/m"
+n_dir="/home/workspace/SWE_2021_41_2023_2_week_4-main/n"
+o_dir="/home/workspace/SWE_2021_41_2023_2_week_4-main/o"
+p_dir="/home/workspace/SWE_2021_41_2023_2_week_4-main/p"
+q_dir="/home/workspace/SWE_2021_41_2023_2_week_4-main/q"
+r_dir="/home/workspace/SWE_2021_41_2023_2_week_4-main/r"
+s_dir="/home/workspace/SWE_2021_41_2023_2_week_4-main/s"
+t_dir="/home/workspace/SWE_2021_41_2023_2_week_4-main/t"
+u_dir="/home/workspace/SWE_2021_41_2023_2_week_4-main/u"
+v_dir="/home/workspace/SWE_2021_41_2023_2_week_4-main/v"
+w_dir="/home/workspace/SWE_2021_41_2023_2_week_4-main/w"
+x_dir="/home/workspace/SWE_2021_41_2023_2_week_4-main/x"
+y_dir="/home/workspace/SWE_2021_41_2023_2_week_4-main/y"
+z_dir="/home/workspace/SWE_2021_41_2023_2_week_4-main/z"
+
+mv "$files_dir"/[aA]* "$a_dir"/
+mv "$files_dir"/[bB]* "$b_dir"/
+mv "$files_dir"/[cC]* "$c_dir"/
+mv "$files_dir"/[dD]* "$d_dir"/
+mv "$files_dir"/[eE]* "$e_dir"/
+mv "$files_dir"/[fF]* "$f_dir"/
+mv "$files_dir"/[gG]* "$g_dir"/
+mv "$files_dir"/[hH]* "$h_dir"/
+mv "$files_dir"/[iI]* "$i_dir"/
+mv "$files_dir"/[jJ]* "$j_dir"/
+mv "$files_dir"/[kK]* "$k_dir"/
+mv "$files_dir"/[lL]* "$l_dir"/
+mv "$files_dir"/[mM]* "$m_dir"/
+mv "$files_dir"/[nN]* "$n_dir"/
+mv "$files_dir"/[oO]* "$o_dir"/
+mv "$files_dir"/[pP]* "$p_dir"/
+mv "$files_dir"/[qQ]* "$q_dir"/
+mv "$files_dir"/[rR]* "$r_dir"/
+mv "$files_dir"/[sS]* "$s_dir"/
+mv "$files_dir"/[tT]* "$t_dir"/
+mv "$files_dir"/[uU]* "$u_dir"/
+mv "$files_dir"/[vV]* "$v_dir"/
+mv "$files_dir"/[wW]* "$w_dir"/
+mv "$files_dir"/[xX]* "$x_dir"/
+mv "$files_dir"/[yY]* "$y_dir"/
+mv "$files_dir"/[zZ]* "$z_dir"/
